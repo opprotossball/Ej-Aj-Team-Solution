7c332a447d3006b6d442fe53efdef481a9d37dca
Jan Stachurski <jan.stachurski@outlook.com>
2024-03-16 16:02:49 +0100

    augmentations

---
+++ src/augmentation.py
@@ -0,0 +1,16 @@
+from imgaug import augmenters as iaa
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+def augment(dataset, n):
+    aug = iaa.Sequential([
+        iaa.AddToHueAndSaturation((-20, 20)), 
+        iaa.Sometimes(0.5, iaa.GaussianBlur(sigma=(0, 0.5))),
+    ])
+    imgarr = np.array([np.array(img.convert('RGB')) for img in dataset.imgs])
+    all_augmented_imgs = [aug(images=imgarr) for _ in range(n)]
+    all_augmented_imgs.append(imgarr)  
+    augmented_imgs = np.concatenate(all_augmented_imgs, axis=0)
+    labels = np.tile(dataset.labels, n + 1)
+    return augmented_imgs, labels
--- src/main.py
+++ src/main.py
@@ -1,4 +1,12 @@
 from utils import model_stealing_submission
+from taskdataset import TaskDataset
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from augmentation import augment
 
 if __name__ == '__main__':
-    print('heLLo')
+    d = torch.load('../data/ModelStealingPub.pt')
+    res = augment(d, 10)
+    print(len(d.imgs))
+    print(len(res[0]))
