4e6bc5fefa5713cff7e50a144f48098b40f8ad83
Jan Stachurski <jan.stachurski@outlook.com>
2024-03-16 12:59:45 +0100

    Initial

---
+++ src/main.py
@@ -0,0 +1,4 @@
+from utils import model_stealing_submission
+
+if __name__ == '__main__':
+    print('heLLo')
---
+++ src/utils.py
@@ -0,0 +1,55 @@
+import requests
+
+token = 'qnVfXxPZmzZ.....' # bez 5 ostatnich znaków
+server_url = ''
+
+def model_stealing_submission(path_to_onnx_file: str):
+    SERVER_URL = server_url
+    ENDPOINT = "/modelstealing/submit"
+    URL = SERVER_URL + ENDPOINT
+
+    TEAM_TOKEN = token
+
+    with open(path_to_onnx_file, "rb") as onnx_file:
+        response = requests.post(
+            URL, files={"file": onnx_file}, headers={"token": TEAM_TOKEN}
+        )
+
+        if response.status_code == 200:
+            return response.content["score"]
+        else:
+            raise Exception(f"Request failed. Status code: {response.status_code}, content: {response.content}")
+        
+def model_stealing(path_to_png_file: str):
+    SERVER_URL = server_url
+    ENDPOINT = "/modelstealing"
+    URL = SERVER_URL + ENDPOINT
+
+    TEAM_TOKEN = token
+
+    with open(path_to_png_file, "rb") as img_file:
+        response = requests.get(
+            URL, files={"file": img_file}, headers={"token": TEAM_TOKEN}
+        )
+
+        if response.status_code == 200:
+            return response.content["representation"]
+        else:
+            raise Exception(f"Request failed. Status code: {response.status_code}, content: {response.content}")
+
+# def model_stealing_reset():
+#     SERVER_URL = server_url
+#     ENDPOINT = "/modelstealing/reset"
+#     URL = SERVER_URL + ENDPOINT
+
+#     TEAM_TOKEN = "[paste your team token here]"
+
+#     response = requests.post(
+#         URL, headers={"token": TEAM_TOKEN}
+#     )
+
+#     if response.status_code == 200:
+#         print("Endpoint rested successfully")
+#     else:
+#         raise Exception(f"Request failed. Status code: {response.status_code}, content: {response.content}")
+        
