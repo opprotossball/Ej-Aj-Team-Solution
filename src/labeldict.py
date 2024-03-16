0bfff2f4074bfc8e8949e61d23a5f09dd5911251
Jan Stachurski <jan.stachurski@outlook.com>
2024-03-16 17:08:47 +0100

    labeldict

---
+++ src/labeldict.py
@@ -0,0 +1,18 @@
+class LabelDict:
+
+    def __init__(self, labels):
+        self.codes2labels_dct = []
+        self.labels2codes_dct = {}
+        i = 0
+        for l in labels:
+            if not self.labels2codes_dct.get(l):
+                self.labels2codes_dct[l] = i
+                i += 1
+                self.codes2labels_dct.append(l)
+
+    def codes2labels(self, cds):
+        return [self.codes2labels_dct[c] for c in cds]
+    
+    def labels2codes(self, lbls):
+        return [self.labels2codes_dct[l] for l in lbls]
+    
